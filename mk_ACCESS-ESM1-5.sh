51e8a5b8dfe364e36a5a5e9d21e09abcacb5620f
Aditi Modi <aditimodi91@gmail.com>
2026-02-09 17:39:50 +0530

    Update file paths for data processing in script

--- mk_ACCESS-ESM1-5.sh
+++ mk_ACCESS-ESM1-5.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -xe
 
-#RAW_DIR="../../raw/no3/UKESM1-0-LL"
-#CLEAN_DIR="../../clean/no3/UKESM1-0-LL"
+ipath="/home/users/aditi/CMIP6/data"
 
 CDO="cdo -O -P 8 -f nc"
 
-$CDO -remapbil,r360x180 -selyear,1950/2014 -mergetime ../../raw/dfe/ACCESS-ESM1-5/dfeos_Omon_ACCESS-ESM1-5_historical*.nc ../../clean/dfe/dfeos_ACCESS-ESM1-5_hist_1950-2014_1deg.nc
-$CDO -sellevidx,1 ../../clean/dfe/dfeos_ACCESS-ESM1-5_hist_1950-2014_1deg.nc ../../clean/dfe/dfeos_ACCESS-ESM1-5_hist_1950-2014_1deg_surf.nc
-$CDO -sellevidx,1/20 ../../clean/dfe/dfeos_ACCESS-ESM1-5_hist_1950-2014_1deg.nc ../../clean/dfe/dfeos_ACCESS-ESM1-5_hist_1950-2014_0-200.nc
-rm ../../clean/dfe/dfeos_ACCESS-ESM1-5_hist_1950-2014_1deg.nc
+$CDO -remapbil,r360x180 -selyear,1950/2014 -mergetime $ipath/Fe/raw/ACCESS-ESM1-5/dfeos_Omon_ACCESS-ESM1-5_historical*.nc $ipath/Fe/clean/dfeos_ACCESS-ESM1-5_hist_1950-2014_1deg.nc
+$CDO -sellevidx,1 $ipath/Fe/clean/dfeos_ACCESS-ESM1-5_hist_1950-2014_1deg.nc $ipath/Fe/clean/dfeos_ACCESS-ESM1-5_hist_1950-2014_1deg_surf.nc
+$CDO -sellevidx,1/20 $ipath/Fe/clean/dfeos_ACCESS-ESM1-5_hist_1950-2014_1deg.nc $ipath/Fe/clean/dfeos_ACCESS-ESM1-5_hist_1950-2014_0-200.nc
+rm $ipath/Fe/clean/dfeos_ACCESS-ESM1-5_hist_1950-2014_1deg.nc
