4fe1b28d9025484255c87a8e24f92293995b9aeb
Shravani Deshmukh <shravani.d2003@gmail.com>
2026-02-09 22:47:06 +0530

    Update file paths in mk_CESM2-WACCM.sh

--- mk_CESM2-WACCM.sh
+++ mk_CESM2-WACCM.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -xe
 
-#RAW_DIR="../../raw/o2/UKESM1-0-LL"
-#CLEAN_DIR="../../clean/o2/UKESM1-0-LL"
+ipath="/home/users/aditi/CMIP6/data"
 
 CDO="cdo -O -P 8 -f nc"
 
-$CDO -remapbil,r360x180 -selyear,1950/2014 -mergetime ../../raw/Nitrate/CESM2-WACCM/no3_Omon_CESM2-WACCM_historical*.nc ../../clean/Nitrate/no3_CESM2-WACCM_hist_1950-2014_1deg.nc
-$CDO -sellevidx,1 ../../clean/Nitrate/no3_CESM2-WACCM_hist_1950-2014_1deg.nc ../../clean/Nitrate/no3_CESM2-WACCM_hist_1950-2014_1deg_surf.nc
-$CDO -sellevidx,1/20 ../../clean/Nitrate/no3_CESM2-WACCM_hist_1950-2014_1deg.nc ../../clean/Nitrate/no3_CESM2-WACCM_hist_1950-2014_0-200.nc
-rm ../../clean/Nitrate/no3_CESM2-WACCM_hist_1950-2014_1deg.nc
+$CDO -remapbil,r360x180 -selyear,1950/2014 -mergetime $ipath/Nitrate/raw/CESM2-WACCM/no3_Omon_CESM2-WACCM_historical*.nc ipath/Nitrate/clean/no3_CESM2-WACCM_hist_1950-2014_1deg.nc
+$CDO -sellevidx,1 ipath/Nitrate/clean/no3_CESM2-WACCM_hist_1950-2014_1deg.nc ipath/Nitrate/clean/no3_CESM2-WACCM_hist_1950-2014_1deg_surf.nc
+$CDO -sellevidx,1/20 ipath/Nitrate/clean/no3_CESM2-WACCM_hist_1950-2014_1deg.nc ipath/Nitrate/clean/no3_CESM2-WACCM_hist_1950-2014_0-200.nc
+rm ipath/Nitrate/clean/no3_CESM2-WACCM_hist_1950-2014_1deg.nc
