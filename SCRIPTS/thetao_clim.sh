911f593c4c7626588d1072d9f7ef710e247b5899
Shravani Deshmukh <shravani.d2003@gmail.com>
2026-02-24 00:10:54 +0530

    create

---
+++ SCRIPTS/thetao_clim.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+set -xe
+
+CDO="cdo -O -P 8"
+
+ipath="../../clean/thetao"
+opath="../data/climatology/thetao"
+
+$CDO -sellonlatbox,30,120,-30,30 -timmean -selyear,1988/2014 $ipath/thetao_ACCESS-ESM1-5_*surf.nc \
+         $opath/thetao_ACCESS-ESM1-5_hist_1988-2014_1deg_surf_IO_timmean.nc
+
+$CDO -sellonlatbox,30,120,-30,30 -timmean -selyear,1988/2014 $ipath/thetao_CanESM5_*surf.nc \
+         $opath/thetao_CanESM5_hist_1988-2014_1deg_surf_IO_timmean.nc
+
+$CDO -sellonlatbox,30,120,-30,30 -timmean -selyear,1988/2014 $ipath/thetao_CanESM5-1_*surf.nc \
+         $opath/thetao_CanESM5-1_hist_1988-2014_1deg_surf_IO_timmean.nc
+
+$CDO -sellonlatbox,30,120,-30,30 -timmean -selyear,1988/2014 $ipath/thetao_CESM2_*surf.nc \
+         $opath/thetao_CESM2_hist_1988-2014_1deg_surf_IO_timmean.nc
+
+$CDO -sellonlatbox,30,120,-30,30 -timmean -selyear,1988/2014 $ipath/thetao_CESM2-FV2_*surf.nc \
+         $opath/thetao_CESM2-FV2_hist_1988-2014_1deg_surf_IO_timmean.nc
+
+$CDO -sellonlatbox,30,120,-30,30 -timmean -selyear,1988/2014 $ipath/thetao_CESM2-WACCM_*surf.nc \
+         $opath/thetao_CESM2-WACCM_hist_1988-2014_1deg_surf_IO_timmean.nc
+
+$CDO -sellonlatbox,30,120,-30,30 -timmean -selyear,1988/2014 $ipath/thetao_CESM2-WACCM-FV2_*surf.nc \
+         $opath/thetao_CESM2-WACCM-FV2_hist_1988-2014_1deg_surf_IO_timmean.nc
+
+$CDO -sellonlatbox,30,120,-30,30 -timmean -selyear,1988/2014 $ipath/thetao_CMCC-ESM2_*surf.nc \
+         $opath/thetao_CMCC-ESM2_hist_1988-2014_1deg_surf_IO_timmean.nc
+
+$CDO -sellonlatbox,30,120,-30,30 -timmean -selyear,1988/2014 $ipath/thetao_CNRM-ESM2-1_*surf.nc \
+         $opath/thetao_CNRM-ESM2-1_hist_1988-2014_1deg_surf_IO_timmean.nc
+
+$CDO -sellonlatbox,30,120,-30,30 -timmean -selyear,1988/2014 $ipath/thetao_GFDL-CM4_*surf.nc \
+         $opath/thetao_GFDL-CM4_hist_1988-2014_1deg_surf_IO_timmean.nc
+
+$CDO -sellonlatbox,30,120,-30,30 -timmean -selyear,1988/2014 $ipath/thetao_GFDL-ESM4_*surf.nc \
+         $opath/thetao_GFDL-ESM4_hist_1988-2014_1deg_surf_IO_timmean.nc
+
+$CDO -sellonlatbox,30,120,-30,30 -timmean -selyear,1988/2014 $ipath/thetao_IPSL-CM5A2-INCA_*surf.nc \
+         $opath/thetao_IPSL-CM5A2-INCA_hist_1988-2014_1deg_surf_IO_timmean.nc
+
+$CDO -sellonlatbox,30,120,-30,30 -timmean -selyear,1988/2014 $ipath/thetao_IPSL-CM6A-LR_*surf.nc \
+         $opath/thetao_IPSL-CM6A-LR_hist_1988-2014_1deg_surf_IO_timmean.nc
+
+$CDO -sellonlatbox,30,120,-30,30 -timmean -selyear,1988/2014 $ipath/thetao_IPSL-CM6A-LR-INCA_*surf.nc \
+         $opath/thetao_IPSL-CM6A-LR-INCA_hist_1988-2014_1deg_surf_IO_timmean.nc
+         
+$CDO -sellonlatbox,30,120,-30,30 -timmean -selyear,1988/2014 $ipath/thetao_MIROC-ES2L_*surf.nc \
+         $opath/thetao_MIROC-ES2L_hist_1988-2014_1deg_surf_IO_timmean.nc
+
+$CDO -sellonlatbox,30,120,-30,30 -timmean -selyear,1988/2014 $ipath/thetao_MPI-ESM-1-2-HAM_*surf.nc \
+         $opath/thetao_MPI-ESM-1-2-HAM_hist_1988-2014_1deg_surf_IO_timmean.nc
+
+$CDO -sellonlatbox,30,120,-30,30 -timmean -selyear,1988/2014 $ipath/thetao_MPI-ESM1-2-HR_*surf.nc \
+         $opath/thetao_MPI-ESM1-2-HR_hist_1988-2014_1deg_surf_IO_timmean.nc
+
+$CDO -sellonlatbox,30,120,-30,30 -timmean -selyear,1988/2014 $ipath/thetao_MPI-ESM1-2-LR_*surf.nc \
+         $opath/thetao_MPI-ESM1-2-LR_hist_1988-2014_1deg_surf_IO_timmean.nc
+
+$CDO -sellonlatbox,30,120,-30,30 -timmean -selyear,1988/2014 $ipath/thetao_NorESM2-LM_*surf.nc \
+         $opath/thetao_NorESM2-LM_hist_1988-2014_1deg_surf_IO_timmean.nc
+
+$CDO -sellonlatbox,30,120,-30,30 -timmean -selyear,1988/2014 $ipath/thetao_NorESM2-MM_*surf.nc \
+         $opath/thetao_NorESM2-MM_hist_1988-2014_1deg_surf_IO_timmean.nc
+
+$CDO -sellonlatbox,30,120,-30,30 -timmean -selyear,1988/2014 $ipath/thetao_UKESM1-0-LL_*surf.nc \
+         $opath/thetao_UKESM1-0-LL_hist_1988-2014_1deg_surf_IO_timmean.nc
