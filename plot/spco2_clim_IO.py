16e3c20a37b7a4ed9117c5b6b1dca2cf6a29f1e3
Shravani Deshmukh <shravani.d2003@gmail.com>
2026-02-23 17:09:45 +0530

    updates

--- plot/spco2_clim_IO.py
+++ plot/spco2_clim_IO.py
@@ -10,27 +10,28 @@ data_path = '../data/climatology/spco2/'
 fig_path  = '../figs/'
 
 files = [
-    'spco2_ACCESS-ESM1-5_hist_1988-2014_1deg_timmean.nc',
-    'spco2_CanESM5_hist_1988-2014_1deg_timmean.nc',
-    'spco2_CanESM5-1_hist_1988-2014_1deg_timmean.nc',
-    'spco2_CESM2_hist_1988-2014_1deg_timmean.nc',
-    'spco2_CESM2-FV2_hist_1988-2014_1deg_timmean.nc',
-    'spco2_CESM2-WACCM_hist_1988-2014_1deg_timmean.nc',
-    'spco2_CESM2-WACCM-FV2_hist_1988-2014_1deg_timmean.nc',
-    'spco2_CMCC-ESM2_hist_1988-2014_1deg_timmean.nc',
-    'spco2_CMCC-ESM2_hist_1988-2014_1deg_timmean.nc', #CNRM-ESM2-1
-    'spco2_GFDL-CM4_hist_1988-2014_1deg_timmean.nc',
-    'spco2_GFDL-ESM4_hist_1988-2014_1deg_timmean.nc',
-    'spco2_GFDL-ESM4_hist_1988-2014_1deg_timmean.nc', #IITM-ESM
-    'spco2_IPSL-CM6A-LR_hist_1988-2014_1deg_timmean.nc', #IPSL-CM5A2-INCA ABSENT
-    'spco2_IPSL-CM6A-LR-INCA_hist_1988-2014_1deg_timmean.nc',
-    'spco2_IPSL-CM6A-LR-INCA_hist_1988-2014_1deg_timmean.nc', #MIROC-ES2L
-    'spco2_MPI-ESM-1-2-HAM_hist_1988-2014_1deg_timmean.nc',
-    'spco2_MPI-ESM1-2-HR_hist_1988-2014_1deg_timmean.nc',
-    'spco2_MPI-ESM1-2-LR_hist_1988-2014_1deg_timmean.nc',
-    'spco2_MPI-ESM1-2-LR_hist_1988-2014_1deg_timmean.nc', #NORESM2-LM, spco2nat?
-    'spco2_NorESM2-MM_hist_1988-2014_1deg_timmean.nc',
-    'spco2_NorESM2-MM_hist_1988-2014_1deg_timmean.nc', #UKESM1-0-LL
+    'spco2_ACCESS-ESM1-5_hist_1988-2014_1deg_IO_timmean.nc',
+    'spco2_CanESM5_hist_1988-2014_1deg_IO_timmean.nc',
+    'spco2_CanESM5-1_hist_1988-2014_1deg_IO_timmean.nc',
+    'spco2_CESM2_hist_1988-2014_1deg_IO_timmean.nc',
+    'spco2_CESM2-FV2_hist_1988-2014_1deg_IO_timmean.nc',
+    'spco2_CESM2-WACCM_hist_1988-2014_1deg_IO_timmean.nc',
+    'spco2_CESM2-WACCM-FV2_hist_1988-2014_1deg_IO_timmean.nc',
+    'spco2_CMCC-ESM2_hist_1988-2014_1deg_IO_timmean.nc',
+    'spco2_CNRM-ESM2-1_hist_1988-2014_1deg_IO_timmean.nc', #CNRM-ESM2-1
+    'spco2_GFDL-CM4_hist_1988-2014_1deg_IO_timmean.nc',
+    'spco2_GFDL-ESM4_hist_1988-2014_1deg_IO_timmean.nc',
+    #'spco2_GFDL-ESM4_hist_1988-2014_1deg_IO_timmean.nc', #IITM-ESM
+    'spco2_IPSL-CM6A-LR_hist_1988-2014_1deg_IO_timmean.nc', #IPSL-CM5A2-INCA ABSENT
+    'spco2_IPSL-CM6A-LR-INCA_hist_1988-2014_1deg_IO_timmean.nc',
+    'spco2_MIROC-ES2L_hist_1988-2014_1deg_IO_timmean.nc', #MIROC-ES2L
+    'spco2_MPI-ESM-1-2-HAM_hist_1988-2014_1deg_IO_timmean.nc',
+    'spco2_MPI-ESM1-2-HR_hist_1988-2014_1deg_IO_timmean.nc',
+    'spco2_MPI-ESM1-2-LR_hist_1988-2014_1deg_IO_timmean.nc',
+    'spco2nat_NorESM2-LM_hist_1988-2014_1deg_IO_timmean.nc', #NORESM2-LM, spco2nat?
+    'spco2_NorESM2-MM_hist_1988-2014_1deg_IO_timmean.nc',
+    'spco2_UKESM1-0-LL_hist_1988-2014_1deg_IO_timmean.nc'#UKESM1-0-LL
+    
 ]
 
 labels = [
@@ -45,7 +46,7 @@ labels = [
     'CNRM-ESM2-1',
     'GFDL-CM4',
     'GFDL-ESM4',
-    'IITM-ESM',
+    #'IITM-ESM',
     'IPSL-CM6A-LR',
     'IPSL-CM6A-LR-INCA',
     'MIROC-ES2L',
@@ -132,5 +133,5 @@ plt.suptitle(
 )
 
 plt.tight_layout(rect=[0, 0, 0.9, 0.93])
-plt.savefig(fig_path + 'spco2_clim_IO1.png', dpi=300)
+plt.savefig(fig_path + 'spco2_clim.png', dpi=300)
 plt.show()
