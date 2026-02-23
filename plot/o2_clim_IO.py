29d7e4093310f7cf1692e9dc2956e263040cda2c
Shravani Deshmukh <shravani.d2003@gmail.com>
2026-02-23 16:38:59 +0530

    updates

--- plot/o2_clim_IO.py
+++ plot/o2_clim_IO.py
@@ -9,24 +9,24 @@ fig_path  = '../figs/'
 
 files = [
 
-    'o2_ACCESS-ESM1-5_hist_1988-2014_1deg_surf_timmean.nc',
-    'o2_CanESM5_hist_1988-2014_1deg_surf_timmean.nc',
-    'o2_CanESM5-1_hist_1988-2014_1deg_surf_timmean.nc',
-    'o2_CMCC-ESM2_hist_1988-2014_1deg_surf_timmean.nc',
-    'o2_CMCC-ESM2_hist_1988-2014_1deg_surf_timmean.nc',#CNRM-ESM2-1
-    'o2_GFDL-CM4_hist_1988-2014_1deg_surf_timmean.nc',
-    'o2_GFDL-ESM4_hist_1988-2014_1deg_surf_timmean.nc',
-    'o2_GFDL-ESM4_hist_1988-2014_1deg_surf_timmean.nc',#IITM-ESM
-    'o2_IPSL-CM5A2-INCA_hist_1988-2014_1deg_surf_timmean.nc',
-    'o2_IPSL-CM6A-LR_hist_1988-2014_1deg_surf_timmean.nc',
-    'o2_IPSL-CM6A-LR-INCA_hist_1988-2014_1deg_surf_timmean.nc',
-    'o2_IPSL-CM6A-LR-INCA_hist_1988-2014_1deg_surf_timmean.nc',#MIROC-ES2L
-    'o2_MPI-ESM-1-2-HAM_hist_1988-2014_1deg_surf_timmean.nc',
-    'o2_MPI-ESM1-2-HR_hist_1988-2014_1deg_surf_timmean.nc',
-    'o2_MPI-ESM1-2-LR_hist_1988-2014_1deg_surf_timmean.nc',
-    'o2_MPI-ESM1-2-LR_hist_1988-2014_1deg_surf_timmean.nc',#NorESM2-LM
-    'o2_NorESM2-MM_hist_1988-2014_1deg_surf_timmean.nc',
-    'o2_NorESM2-MM_hist_1988-2014_1deg_surf_timmean.nc'#UKESM1-0-LL
+    'o2_ACCESS-ESM1-5_hist_1988-2014_1deg_surf_IO_timmean.nc',
+    'o2_CanESM5_hist_1988-2014_1deg_surf_IO_timmean.nc',
+    'o2_CanESM5-1_hist_1988-2014_1deg_surf_IO_timmean.nc',
+    'o2_CMCC-ESM2_hist_1988-2014_1deg_surf_IO_timmean.nc',
+    'o2_CNRM-ESM2-1_hist_1988-2014_1deg_surf_IO_timmean.nc',#CNRM-ESM2-1
+    'o2_GFDL-CM4_hist_1988-2014_1deg_surf_IO_timmean.nc',
+    'o2_GFDL-ESM4_hist_1988-2014_1deg_surf_IO_timmean.nc',
+    #'o2_GFDL-ESM4_hist_1988-2014_1deg_surf_timmean.nc',#IITM-ESM
+    'o2_IPSL-CM5A2-INCA_hist_1988-2014_1deg_surf_IO_timmean.nc',
+    'o2_IPSL-CM6A-LR_hist_1988-2014_1deg_surf_IO_timmean.nc',
+    'o2_IPSL-CM6A-LR-INCA_hist_1988-2014_1deg_surf_IO_timmean.nc',
+    'o2_MIROC-ES2L_hist_1988-2014_1deg_surf_IO_timmean.nc',#MIROC-ES2L
+    'o2_MPI-ESM-1-2-HAM_hist_1988-2014_1deg_surf_IO_timmean.nc',
+    'o2_MPI-ESM1-2-HR_hist_1988-2014_1deg_surf_IO_timmean.nc',
+    'o2_MPI-ESM1-2-LR_hist_1988-2014_1deg_surf_IO_timmean.nc',
+    'o2os_NorESM2-LM_hist_1988-2014_1deg_surf_IO_timmean.nc',#NorESM2-LM
+    'o2_NorESM2-MM_hist_1988-2014_1deg_surf_IO_timmean.nc',
+    'o2_UKESM1-0-LL_hist_1988-2014_1deg_surf_IO_timmean.nc'#UKESM1-0-LL
 ]
 
 labels = [
@@ -38,7 +38,7 @@ labels = [
     'CNRM-ESM2-1',
     'GFDL-CM4',
     'GFDL-ESM4',
-    'IITM-ESM',
+    #'IITM-ESM',
     'IPSL-CM5A2-INCA',
     'IPSL-CM6A-LR',
     'IPSL-CM6A-LR-INCA',
@@ -129,5 +129,5 @@ plt.suptitle(
 )
 
 plt.tight_layout(rect=[0, 0, 0.9, 0.93])
-plt.savefig(fig_path + 'o2_clim_IO.png', dpi=300)
+plt.savefig(fig_path + 'o2_clim.png', dpi=300)
 plt.show()
