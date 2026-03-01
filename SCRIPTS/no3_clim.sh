29ae46172551b9a7bda3a9888257abe62d8d87a4
Shravani Deshmukh <shravani.d2003@gmail.com>
2026-03-01 17:57:46 +0530

    Update no3_clim.sh

--- SCRIPTS/no3_clim.sh
+++ SCRIPTS/no3_clim.sh
@@ -62,7 +62,7 @@ $CDO -sellonlatbox,30,120,-30,30 -mulc,1000 -timmean -selyear,1988/2014 $ipath/n
           $opath/no3_NorESM2-MM_hist_1988-2014_1deg_surf_IO_timmean.nc
 
 $CDO -sellonlatbox,30,120,-30,30 -mulc,1000 -timmean -selyear,1988/2014 $ipath/no3os_NorESM2-LM_*.nc \
-          $opath/no3os_NorESM2-LM_hist_1988-2014_1deg_surf_IO_timmean.nc
+          $opath/no3_NorESM2-LM_hist_1988-2014_1deg_surf_IO_timmean.nc
 
 $CDO -sellonlatbox,30,120,-30,30 -mulc,1000 -timmean -selyear,1988/2014 $ipath/no3_UKESM1-0-LL_*surf.nc \
           $opath/no3_UKESM1-0-LL_hist_1988-2014_1deg_surf_IO_timmean.nc
