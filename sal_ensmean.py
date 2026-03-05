02cb49911c0b576a7882c0a019f065c28e3b9c84
Shravani Deshmukh <shravani.d2003@gmail.com>
2026-03-05 07:31:29 +0530

    Create sal_ensmean.py

---
+++ sal_ensmean.py
@@ -0,0 +1,118 @@
+import xarray as xr
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+
+data_path_obs = '../../../observations_bgc/sal/salanity_ORAS5_1998-2024_1deg_surf_IO_clim.nc'
+data_path_esm = '../data/climatology/sal/ensmean_sal.nc'
+fig_path  = '../figs/'
+
+files = [data_path_obs, data_path_esm]
+
+labels = [
+    'ORAS5',
+    'Ensemble Mean'
+]
+
+bounds = [32, 32.5, 33, 33.5, 34, 34.5, 35, 35.5,
+          36, 36.5, 37, 37.7, 38]
+
+colors = [
+    "#08306B", 
+    "#08519C",
+    "#2171B5",
+    "#4292C6",
+    "#6BAED6",
+    "#9ECAE1",
+    "#C6DBEF",
+    "#E0F3F8",
+    "#FFFFCC",
+    "#FFEDA0",
+    "#FED976",
+    "#FEB24C",
+    "#FD8D3C"
+]
+
+cmap = mcolors.ListedColormap(colors)
+norm = mcolors.BoundaryNorm(bounds, cmap.N, extend='max')
+
+ncols = 2
+nrows = 1
+
+latitude_ticks  = [-30, -15, 0, 15, 30]
+longitude_ticks = [30, 60, 90, 120]
+
+fig, axes = plt.subplots(
+    nrows=nrows,
+    ncols=ncols,
+    figsize=(12, 5),
+    subplot_kw={'projection': ccrs.PlateCarree()}
+)
+
+
+axes = axes.flatten()
+
+
+for i, filepath in enumerate(files):
+    ax = axes[i]
+    filename = files[i]
+
+    ds = xr.open_dataset(filepath, decode_times=False)
+    sal = ds['so']
+    
+   
+    plot = sal.plot(
+           ax=ax,
+           cmap=cmap,
+           norm=norm,
+           add_colorbar=False,
+           transform=ccrs.PlateCarree()
+           )
+
+   contour_levels = [32.5, 34.5, 36.5]
+
+    cs = ax.contour(
+        sal.lon,
+        sal.lat,
+        sal,
+        levels=contour_levels,
+        colors='black',
+        linewidths=0.8,
+        transform=ccrs.PlateCarree()
+    )
+
+    ax.clabel(
+        cs,
+        inline=True,
+        fontsize=7,
+        fmt='%1.1f' #in point-form
+    )
+
+    ax.add_feature(cfeature.COASTLINE, linewidth=0.8)
+    ax.add_feature(cfeature.LAND, facecolor='lightgrey')
+
+    ax.set_xticks(longitude_ticks, crs=ccrs.PlateCarree())
+    ax.set_yticks(latitude_ticks, crs=ccrs.PlateCarree())
+
+    from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
+    ax.xaxis.set_major_formatter(LongitudeFormatter(zero_direction_label=True))
+    ax.yaxis.set_major_formatter(LatitudeFormatter())
+
+    ax.tick_params(axis='x', labelsize=8)
+    ax.tick_params(axis='y', labelsize=8)
+
+    ax.set_title(labels[i], fontsize=11)
+    ax.set_xlabel('')
+    ax.set_ylabel('')
+
+
+cax = fig.add_axes([0.92, 0.22, 0.015, 0.56])
+cbar = plt.colorbar(plot, cax=cax, ticks=bounds)
+cbar.set_label('Ocean Surface Salinity (psu)')
+
+
+plt.tight_layout(rect=[0, 0, 0.9, 0.93])
+
+plt.savefig(fig_path + 'sal_ensmean_occi.png', dpi=300)
+plt.show()
