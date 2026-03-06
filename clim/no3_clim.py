1e47e5f8164233fa120e1f555135b0ba4f2087cb
Shravani Deshmukh <shravani.d2003@gmail.com>
2026-03-06 23:59:56 +0530

    Update no3_clim.py

--- clim/no3_clim.py
+++ clim/no3_clim.py
@@ -8,27 +8,27 @@ data_path = '../data/climatology/no3/'
 fig_path  = '../figs/'
 
 files = [
-    'no3_ACCESS-ESM1-5_hist_1988-2014_1deg_surf_timmean.nc',
-    'no3_CanESM5_hist_1988-2014_1deg_surf_timmean.nc',
-    'no3_CanESM5-1_hist_1988-2014_1deg_surf_timmean.nc',
-    'no3_CESM2_hist_1988-2014_1deg_surf_timmean.nc',
-    'no3_CESM2-FV2_hist_1988-2014_1deg_surf_timmean.nc',
-    'no3_CESM2-WACCM_hist_1988-2014_1deg_surf_timmean.nc', 
-    'no3_CESM2-WACCM-FV2_hist_1988-2014_1deg_surf_timmean.nc',
-    'no3_CMCC-ESM2_hist_1988-2014_1deg_surf_timmean.nc',
-    'no3_CNRM-ESM2-1_hist_1988-2014_1deg_surf_timmean.nc',
-    'no3_GFDL-ESM4_hist_1988-2014_1deg_surf_timmean.nc',
-    #'no3_GFDL-ESM4_hist_1988-2014_1deg_surf_timmean.nc', #IITM-ESM
-    'no3_IPSL-CM5A2-INCA_hist_1988-2014_1deg_surf_timmean.nc',
-    'no3_IPSL-CM6A-LR_hist_1988-2014_1deg_surf_timmean.nc',
-    'no3_IPSL-CM6A-LR-INCA_hist_1988-2014_1deg_surf_timmean.nc',
-    'no3_MIROC-ES2L_hist_1988-2014_1deg_surf_timmean.nc', 
-    'no3_MPI-ESM-1-2-HAM_hist_1988-2014_1deg_surf_timmean.nc',
-    'no3_MPI-ESM1-2-HR_hist_1988-2014_1deg_surf_timmean.nc',
-    'no3_MPI-ESM1-2-LR_hist_1988-2014_1deg_surf_timmean.nc',
-    'no3_NorESM2-LM_hist_1988-2014_1deg_surf_timmean.nc', 
-    'no3_NorESM2-MM_hist_1988-2014_1deg_surf_timmean.nc',
-    'no3_UKESM1-0-LL_hist_1988-2014_1deg_surf_timmean.nc' 
+    'no3_ACCESS-ESM1-5_hist_1988-2014_1deg_surf_IO_timmean.nc',
+    'no3_CanESM5_hist_1988-2014_1deg_surf_IO_timmean.nc',
+    'no3_CanESM5-1_hist_1988-2014_1deg_surf_IO_timmean.nc',
+    'no3_CESM2_hist_1988-2014_1deg_surf_IO_timmean.nc',
+    'no3_CESM2-FV2_hist_1988-2014_1deg_surf_IO_timmean.nc',
+    'no3_CESM2-WACCM_hist_1988-2014_1deg_surf_IO_timmean.nc', 
+    'no3_CESM2-WACCM-FV2_hist_1988-2014_1deg_surf_IO_timmean.nc',
+    'no3_CMCC-ESM2_hist_1988-2014_1deg_surf_IO_timmean.nc',
+    'no3_CNRM-ESM2-1_hist_1988-2014_1deg_surf_IO_timmean.nc',
+    'no3_GFDL-ESM4_hist_1988-2014_1deg_surf_IO_timmean.nc',
+    #'no3_GFDL-ESM4_hist_1988-2014_1deg_surf_IO_timmean.nc', #IITM-ESM
+    'no3_IPSL-CM5A2-INCA_hist_1988-2014_1deg_surf_IO_timmean.nc',
+    'no3_IPSL-CM6A-LR_hist_1988-2014_1deg_surf_IO_timmean.nc',
+    'no3_IPSL-CM6A-LR-INCA_hist_1988-2014_1deg_surf_IO_timmean.nc',
+    'no3_MIROC-ES2L_hist_1988-2014_1deg_surf_IO_timmean.nc', 
+    'no3_MPI-ESM-1-2-HAM_hist_1988-2014_1deg_surf_IO_timmean.nc',
+    'no3_MPI-ESM1-2-HR_hist_1988-2014_1deg_surf_IO_timmean.nc',
+    'no3_MPI-ESM1-2-LR_hist_1988-2014_1deg_surf_IO_timmean.nc',
+    'no3_NorESM2-LM_hist_1988-2014_1deg_surf_IO_timmean.nc', 
+    'no3_NorESM2-MM_hist_1988-2014_1deg_surf_IO_timmean.nc',
+    'no3_UKESM1-0-LL_hist_1988-2014_1deg_surf_IO_timmean.nc' 
 ]
 
 labels = [
