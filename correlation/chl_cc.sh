35eecff3d6c6b50d3903aa06c1cf891f3af0291b
Shravani Deshmukh <shravani.d2003@gmail.com>
2026-03-11 00:10:07 +0530

    Update chl_cc.sh

--- correlation/chl_cc.sh
+++ correlation/chl_cc.sh
@@ -11,103 +11,83 @@ $CDO -timcor -mulc,1e6 -sellonlatbox,30,120,-30,30 -selyear,1988/2014 $esm_path/
       $obs_path \
       $opath/chl_ACCESS-ESM1-5_cc.nc
 
-$CDO -timcor \
-    $esm_path/chl_CanESM5_hist_1988-2014_1deg_IO.nc \
+$CDO -timcor -mulc,1e6 -sellonlatbox,30,120,-30,30 -selyear,1988/2014 $esm_path/chl_CanESM5_*surf.nc \
     $obs_path \
     $opath/chl_CanESM5_cc.nc
 
-$CDO -timcor \
-    $esm_path/chl_CanESM5-1_hist_1988-2014_1deg_IO.nc \
+$CDO -timcor -mulc,1e6 -sellonlatbox,30,120,-30,30 -selyear,1988/2014 $esm_path/chl_CanESM5-1_*surf.nc \
     $obs_path \
     $opath/chl_CanESM5-1_cc.nc
 
-$CDO -timcor \
-     $esm_path/chl_CESM2_hist_1988-2014_1deg_IO.nc \
+$CDO -timcor -mulc,1e6 -sellonlatbox,30,120,-30,30 -selyear,1988/2014 $esm_path/chl_CESM2_*surf.nc \
      $obs_path \
      $opath/chl_CESM2_cc.nc
 
-$CDO -timcor \
-    $esm_path/chl_CESM2-FV2_hist_1988-2014_1deg_IO.nc \
+$CDO -timcor -mulc,1e6 -sellonlatbox,30,120,-30,30 -selyear,1988/2014 $esm_path/chl_CESM2-FV2_*surf.nc \
     $obs_path \
     $opath/chl_CESM2-FV2_cc.nc
 
-$CDO -timcor \
-    $esm_path/chl_CESM2-WACCM_hist_1988-2014_1deg_IO.nc \
+$CDO -timcor -mulc,1e6 -sellonlatbox,30,120,-30,30 -selyear,1988/2014 $esm_path/chl_CESM2-WACCM_*surf.nc \
     $obs_path \
     $opath/chl_CESM2-WACCM_cc.nc
 
-$CDO -timcor \
-    $esm_path/chl_CESM2-WACCM-FV2_hist_1988-2014_1deg_IO.nc \
+$CDO -timcor -mulc,1e6 -sellonlatbox,30,120,-30,30 -selyear,1988/2014 $esm_path/chl_CESM2-WACCM-FV2_*surf.nc \
     $obs_path \
     $opath/chl_CESM2-WACCM-FV2_cc.nc
 
-$CDO -timcor \
-    $esm_path/chl_CMCC-ESM2_hist_1988-2014_1deg_IO.nc \
+$CDO -timcor -mulc,1e6 -sellonlatbox,30,120,-30,30 -selyear,1988/2014 $esm_path/chl_CMCC-ESM2_*surf.nc \
     $obs_path \
     $opath/chl_CMCC-ESM2_cc.nc
 
-$CDO -timcor \
-    $esm_path/chl_CNRM-ESM2-1_hist_1988-2014_1deg_IO.nc \
+$CDO -timcor -mulc,1e3 -sellonlatbox,30,120,-30,30 -selyear,1988/2014 $esm_path/chl_CNRM-ESM2-1_*surf.nc \
     $obs_path \
     $opath/chl_CNRM-ESM2-1_cc.nc
 
-$CDO -timcor \
-    $esm_path/chl_GFDL-CM4_hist_1988-2014_1deg_IO.nc \
+$CDO -timcor -mulc,1e6 -sellonlatbox,30,120,-30,30 -selyear,1988/2014 $esm_path/chl_GFDL-CM4_*surf.nc \
     $obs_path \
     $opath/chl_GFDL-CM4_cc.nc
 
-$CDO -timcor \
-    $esm_path/chl_GFDL-ESM4_hist_1988-2014_1deg_IO.nc \
+$CDO -timcor -mulc,1e6 -sellonlatbox,30,120,-30,30 -selyear,1988/2014 $esm_path/chl_GFDL-ESM4_*surf.nc \
     $obs_path \
     $opath/chl_GFDL-ESM4_cc.nc
 
-$CDO -timcor \
-    $esm_path/chl_IPSL-CM5A2-INCA_hist_1988-2014_1deg_IO.nc \
+$CDO -timcor -mulc,1e3 -sellonlatbox,30,120,-30,30 -selyear,1988/2014 $esm_path/chl_IPSL-CM5A2-INCA_*surf.nc \
     $obs_path \
     $opath/chl_IPSL-CM5A2-INCA_cc.nc
 
-$CDO -timcor \
-    $esm_path/chl_IPSL-CM6A-LR_hist_1988-2014_1deg_IO.nc \
+$CDO -timcor -mulc,1e3 -sellonlatbox,30,120,-30,30 -selyear,1988/2014 $esm_path/chl_IPSL-CM6A-LR_*surf.nc \
     $obs_path \
     $opath/chl_IPSL-CM6A-LR_cc.nc
 
-$CDO -timcor \
-    $esm_path/chl_IPSL-CM6A-LR-INCA_hist_1988-2014_1deg_IO.nc \
+$CDO -timcor -mulc,1e3 -sellonlatbox,30,120,-30,30 -selyear,1988/2014 $esm_path/chl_IPSL-CM6A-LR-INCA_*surf.nc \
     $obs_path \
     $opath/chl_IPSL-CM6A-LR-INCA_cc.nc
 
-$CDO -timcor \
-    $esm_path/chl_MIROC-ES2L_hist_1988-2014_1deg_IO.nc \
+$CDO -timcor -mulc,1e6 -sellonlatbox,30,120,-30,30 -selyear,1988/2014 $esm_path/chl_MIROC-ES2L_*surf.nc \
     $obs_path \
     $opath/chl_MIROC-ES2L_cc.nc
 
-$CDO -timcor \
-    $esm_path/chl_MPI-ESM-1-2-HAM_hist_1988-2014_1deg_IO.nc \
+$CDO -timcor -mulc,1e6 -sellonlatbox,30,120,-30,30 -selyear,1988/2014 $esm_path/chl_MPI-ESM-1-2-HAM_*surf.nc \
     $obs_path \
     $opath/chl_MPI-ESM-1-2-HAM_cc.nc
 
-$CDO -timcor \
-    $esm_path/chl_MPI-ESM1-2-HR_hist_1988-2014_1deg_IO.nc \
+$CDO -timcor -mulc,1e6 -sellonlatbox,30,120,-30,30 -selyear,1988/2014 $esm_path/chl_MPI-ESM1-2-HR_*surf.nc \
     $obs_path \
     $opath/chl_MPI-ESM1-2-HR_cc.nc
 
-$CDO -timcor \
-    $esm_path/chl_MPI-ESM1-2-LR_hist_1988-2014_1deg_IO.nc \
+$CDO -timcor -mulc,1e6 -sellonlatbox,30,120,-30,30 -selyear,1988/2014 $esm_path/chl_MPI-ESM1-2-LR_*surf.nc \
     $obs_path \
     $opath/chl_MPI-ESM1-2-LR_cc.nc
 
-$CDO -timcor \
-    $esm_path/chl_NorESM2-LM_hist_1988-2014_1deg_IO.nc \
+$CDO -timcor -mulc,1e6 -sellonlatbox,30,120,-30,30 -selyear,1988/2014 $esm_path/chl_NorESM2-LM_*surf.nc \
     $obs_path \
     $opath/chl_NorESM2-LM_cc.nc
 
-$CDO -timcor \
-    $esm_path/chl_NorESM2-MM_hist_1988-2014_1deg_IO.nc \
+$CDO -timcor -mulc,1e6 -sellonlatbox,30,120,-30,30 -selyear,1988/2014 $esm_path/chl_NorESM2-MM_*surf.nc \
     $obs_path \
     $opath/chl_NorESM2-MM_cc.nc
 
-$CDO -timcor \
-    $esm_path/chl_UKESM1-0-LL_hist_1988-2014_1deg_IO.nc \
+$CDO -timcor -mulc,1e6 -sellonlatbox,30,120,-30,30 -selyear,1988/2014 $esm_path/chl_UKESM1-0-LL_*surf.nc \
     $obs_path \
     $opath/chl_UKESM1-0-LL_cc.nc
 
